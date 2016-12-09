2c274cf5f92b9ac8f6b44112d2e1b0c676464783
Leemur-dev <--null>
2016-12-09 16:34:47 +0100

    Config fos_user

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             #new AppBundle\AppBundle(),
             new Grd\BlogBundle\GrdBlogBundle(),
             new Travel\BlogBundle\TravelBlogBundle(),
+	    new FOS\UserBundle\FOSUserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
